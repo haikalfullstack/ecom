d085d917553d124f39942d0b677237db88f378ee
ekalrozhan <ekalrozhan@gmail.com>
2023-03-18 16:36:25 +0800

    Profile change password page

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -61,4 +61,8 @@ class AdminController extends Controller
         return redirect()->back()->with($notification);
 
     }
+
+    public function AdminChangePassword(){
+        return view('admin.admin_change_password');
+    }
 }
